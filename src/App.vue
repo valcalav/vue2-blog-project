9b89670480e7f67b0598079568f42f2adfa250fb
Veronica <veronica.alcala@gmail.com>
2021-06-30 16:52:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+
+export default {
+  components: { addBlog }
+  
+}
+</script>
+
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="add-blog">
+      <h2>Add a New Blog Post</h2>
+      <form>
+          <label for="">Blog Title:</label>
+          <input type="text" required />
+          <label for="">Blog Content:</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+      </form>
+      <div id="preview">
+          <h3>Preview Blog</h3>
+          <p>Blog title:</p>
+          <p>Blog content:</p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
